4a163210be137bfbb9b24fd0bb6e67936616747e
HyoKwang Ryu <hyokwang.ryu@yanolja.com>
2022-01-24 12:03:40 +0900

    feat: 테스트 코드 세팅(wip)
    - faker 라이브러리 추가
    - posts service spec 작성

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,15 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreatePostDto } from './dtos/create-post.dto';
 import { PostsService } from './posts.service';
+import * as faker from '@faker-js/faker';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
+
+const mockPostRepository = () => ({
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  delete: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('PostsService', () => {
   let service: PostsService;
+  let postRepository: MockRepository<Post>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: mockPostRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    postRepository = module.get<MockRepository<Post>>(getRepositoryToken(Post));
+  });
+
+  describe('create()', () => {
+    const createPostDto: CreatePostDto = {
+      title: faker.lorem.sentence(),
+      content: faker.lorem.paragraph(),
+    };
+    const user: User = new User()
+
+    it('should create Post', async () => {
+      postRepository.save.mockRejectedValue('save error');
+
+      const result = await service.create(createPostDto, user);
+      expect(result).toEqual('save error');
+    });
   });
 
   it('should be defined', () => {
